573b7fcecffe123c108afd5a8bd918eb24520df8
coneseo <woon4754@gmail.com>
2019-10-15 23:42:47 +0900

    ApplicationLister, ApplicationStartingEvent 추가

---
+++ webservershowcase/src/main/java/com/coneseo/webservershowcase/SimpleListener.java
@@ -0,0 +1,22 @@
+package com.coneseo.webservershowcase;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleListener implements ApplicationListener<ApplicationStartingEvent> {
+
+/**
+ * ApplicationStartingEvent -> 어플리케이션이 실행될 때 실행된다. 그래서 빈으로 등록이 안돼서
+ * 아래처럼 하면 실행이 안됨
+ * 직접 등록을 해줘야한다.
+ */
+
+  @Override
+  public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
+    System.out.println("==========================");
+    System.out.println("Application is starting");
+    System.out.println("=========================");
+  }
+}
