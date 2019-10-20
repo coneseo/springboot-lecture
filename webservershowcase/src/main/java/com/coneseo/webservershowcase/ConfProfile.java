86af6fe861d60034ff39b0a0480412857e233424
coneseo <woon4754@gmail.com>
2019-10-20 15:54:01 +0900

    @Profile("conf")를 이용해서 외부 설정 읽어들이기(2)

---
+++ webservershowcase/src/main/java/com/coneseo/webservershowcase/ConfProfile.java
@@ -0,0 +1,10 @@
+package com.coneseo.webservershowcase;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("prod")
+@Configuration
+public class ConfProfile {
+
+}
